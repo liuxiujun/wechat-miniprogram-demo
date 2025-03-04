5f4d817b5b9c7ea2aa43926f4c8beaae6bd7afbf
liuxiujun <liuxiujun09@outlook.com>
2025-03-05 01:51:57 +0800

    +演示WXML渲染语法

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,33 +3,6 @@ Page({
     data: {
         name: '小明',
         now: app.globalData.now,
+        items: ['事项1', '事项2', '事项3']
     },
-    /*
-    * 同一事件在同一个元素上面会触发两次: 一次捕获, 一次冒泡
-    * 事件捕获: 从外到内 
-    * 事件冒泡: 从内到外
-    */
-    handleClick: function(event) {
-        const that = this;
-        wx.showModal({
-            title: '提示',
-            content: '确定要修改名字吗?',
-            success: function(res) {
-                if (res.confirm) {
-                    that.setData({
-                        name: '小红'
-                    }, function() {
-                        wx.showToast({
-                            title: '名字修改成功',
-                            icon: 'success',
-                            duration: 2000
-                        });
-                    });
-                } 
-                else if (res.cancel) {
-                    console.log('用户点击取消');
-                }
-            }
-        })
-    }
 })
