2e605600590f1b6c829b7389d0cf53e5818b2bbe
liuxiujun <liuxiujun09@outlook.com>
2025-03-05 01:31:45 +0800

    +演示事件

--- pages/home/home.js
+++ pages/home/home.js
@@ -4,4 +4,14 @@ Page({
         name: '小明',
         now: app.globalData.now,
     },
+    /*
+    * 同一事件在同一个元素上面会触发两次: 一次捕获, 一次冒泡
+    * 事件捕获: 从外到内 
+    * 事件冒泡: 从内到外
+    */
+    handleClick: function() {
+        this.setData({
+            name: '小红'
+        })
+    }
 })
